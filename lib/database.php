e825a3474c54f8992d7351c0fc29667a2d5fdcf5
J.S <jimmy.sene@laposte.net>
2020-09-25 17:01:08 +0200

    Refactoring part 2 done

--- lib/database.php
+++ lib/database.php
@@ -11,3 +11,59 @@ function getPdo() : PDO
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
     ]);  
 }
+
+/**
+ * Retourne la liste des articles triés par date de création
+ * @return array
+ */
+function getAllArticles() : array
+{
+    $pdo = getPdo();
+    $resultats = $pdo->query('SELECT * FROM articles ORDER BY created_at DESC');
+    return $resultats->fetchAll();
+}
+
+function getArticle(int $id)
+{
+    $pdo = getPdo();
+    $query = $pdo->prepare("SELECT * FROM articles WHERE id = :article_id");
+    $query->execute(['article_id' => $id]);
+    return $query->fetch();
+}
+
+function getAllComments(int $id) : array
+{
+    $pdo = getPdo();
+    $query = $pdo->prepare("SELECT * FROM comments WHERE article_id = :article_id");
+    $query->execute(['article_id' => $id]);
+    return $query->fetchAll();
+}
+
+function deleteArticle(int $id)
+{
+    $pdo = getPdo();
+    $query = $pdo->prepare('DELETE FROM articles WHERE id = :id');
+    $query->execute(['id' => $id]);
+}
+
+function getComment(int $id)
+{
+    $pdo = getPdo();
+    $query = $pdo->prepare('SELECT * FROM comments WHERE id = :id');
+    $query->execute(['id' => $id]);
+    return $query->fetch();
+}
+
+function deleteComment($id) : void
+{
+    $pdo = getPdo();
+    $query = $pdo->prepare('DELETE FROM comments WHERE id = :id');
+    $query->execute(['id' => $id]);
+}
+
+function insertComment($article_id, $content, $author) : void
+{
+    $pdo = getPdo();
+    $query = $pdo->prepare('INSERT INTO comments SET author = :author, content = :content, article_id = :article_id, created_at = NOW()');
+    $query->execute(compact('author', 'content', 'article_id'));
+}
