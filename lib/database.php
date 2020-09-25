b199190b2f6d55111b7f8be359a75339e2916012
J.S <jimmy.sene@laposte.net>
2020-09-25 20:19:20 +0200

    Singleton PDO connection

--- lib/database.php
+++ lib/database.php
@@ -2,6 +2,7 @@
 
 class Database 
 {
+    private static $instance = null;
     /**
      * Retourne une connexion à la BDD
      * 
@@ -9,9 +10,13 @@ class Database
      */
     public static function getPdo() : PDO
     {
-        return new PDO('mysql:host=localhost;dbname=blogpoo;charset=utf8', 'root', '', [
+        if(!self::$instance)
+        {
+            self::$instance = new PDO('mysql:host=localhost;dbname=blogpoo;charset=utf8', 'root', '', [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-        ]);  
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC]);
+        }
+        return self::$instance;
+         
     }
 }
